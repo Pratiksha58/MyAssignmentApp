aa9c984fbb359c63fa5c7274d5e8458ac9a8af62
Pratiksha Mokashi <pratiksha.mokashi@neosoftmail.com>
2021-10-23 23:20:47 +0530

    initial commit

--- app/src/main/java/com/app/assignmentapp/presentation/view/adapter/DataListAdapter.kt
+++ app/src/main/java/com/app/assignmentapp/presentation/view/adapter/DataListAdapter.kt
@@ -7,11 +7,12 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.app.assignmentapp.R
 import com.app.assignmentapp.data.Responses.ArticleE
+import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.layout_item.view.*
 
 class DataListAdapter(
     var mctx : Context,
-    val afPlayers: List<ArticleE>) :
+    val mList: List<ArticleE>) :
     RecyclerView.Adapter<DataListAdapter.ViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         return ViewHolder(
@@ -24,19 +25,29 @@ class DataListAdapter(
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.name.setText("Name : "+afPlayers[position].name)
-        holder.author_name.setText("Author Name : "+afPlayers[position].author)
-        holder.category_name.setText("Category Name : "+afPlayers[position].categoryName)
+        holder.name.setText("Name : "+mList[position].name)
+        holder.author_name.setText("Author Name : "+mList[position].author)
+        holder.category_name.setText("Category Name : "+mList[position].categoryName)
+        if (mList[position].image != null){
+            Glide.with(mctx)
+                .load(mList[position].image)
+                .into(holder.img_article);
+        }else {
+            Glide.with(mctx)
+                .load(R.drawable.img_medicine)
+                .into(holder.img_article)
+        }
     }
 
     override fun getItemCount(): Int {
-        return afPlayers.size
+        return mList.size
     }
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val author_name = itemView.tv_author_name
         val category_name = itemView.tv_category_name
         val name = itemView.name
+        val img_article = itemView.img_article
 
 
     }
